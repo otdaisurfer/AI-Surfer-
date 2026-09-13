97558847fdc219b45eb6655dfed0e3e5ca55cb4e
🌊⛵AI Surfer🌺Ocean 🏄💦 <oceantidedrop@gmail.com>
2026-09-13 19:00:19 -0400

    style: load landing funnel styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import RouterApp from "./RouterApp";
 import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
+import "./pages/home/SitesLandingFunnel.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
